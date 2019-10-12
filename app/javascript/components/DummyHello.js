3c75114d255512b63512311dffaa0d0acf8a1585
Richard <richbluehill@gmail.com>
2019-10-11 19:25:44 -0700

    added dummy react component at /react_hello/index

---
+++ app/javascript/components/DummyHello.js
@@ -0,0 +1,10 @@
+import React from "react"
+class DummyHello extends React.Component {
+  render () {
+    return (
+      <div> hello {this.props.name}</div>
+    );
+  }
+}
+
+export default DummyHello

cf1288fcf66d0bd20dee4d29e9f26f8e511de096
Kristie Lim <kristie.lim@gmail.com>
2019-10-24 13:18:22 -0700

    Rename to jsx

--- app/javascript/components/DummyHello.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-class DummyHello extends React.Component {
-  render() {
-    return <div> hello {this.props.name}</div>;
-  }
-}
-
-export default DummyHello;
--- app/javascript/components/TicTacMap.jsx
+++ app/javascript/components/TicTacMap.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Map from "pigeon-maps";
-import Marker from "pigeon-marker";
+import React from 'react';
+import Map from 'pigeon-maps';
+import Marker from 'pigeon-marker';
 // import Overlay from "pigeon-overlay";
 
 const TicTacMap = props => {
+  const { currLocation } = props;
   return (
-    // <h1>Hello Map</h1>
-    <Map center={[50.879, 4.6997]} zoom={12} width={600} height={400}>
+    <Map center={currLocation} zoom={12} width={600} height={400}>
       <Marker
         anchor={[50.874, 4.6947]}
         payload={1}

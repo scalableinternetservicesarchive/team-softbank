57580b558cac690fbdb2d189c1ae6f1723b7993f
Kristie Lim <kristie.lim@gmail.com>
2019-10-25 14:50:27 -0700

    Get current location

--- app/javascript/components/TicTacMap.jsx
+++ app/javascript/components/TicTacMap.jsx
@@ -2,30 +2,45 @@ import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 // import Overlay from "pigeon-overlay";
+import { geolocated } from 'react-geolocated';
 
 const TicTacMap = props => {
-  const { currLocation } = props;
-  const [zoom, setZoom] = React.useState(12);
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } = props;
 
-  return (
-    <>
-      <Map
-        mouseEvents={true}
-        center={currLocation}
-        zoom={zoom}
-        width={600}
-        height={400}
-      >
-        <Marker
-          anchor={[50.874, 4.6947]}
-          payload={1}
-          onClick={({ event, anchor, payload }) => {}}
-        />
-      </Map>
-      <button onClick={() => setZoom(zoom + 1)}>Zoom In</button>
-      <button onClick={() => setZoom(zoom - 1)}>Zoom Out</button>
-    </>
-  );
+  const [zoom, setZoom] = React.useState(14);
+  if (!isGeolocationAvailable) {
+    return <div>Your browser does not support Geolocation</div>;
+  } else if (!isGeolocationEnabled) {
+    return <div>Geolocation is not enabled</div>;
+  } else if (coords) {
+    const currLocation = [coords.latitude, coords.longitude];
+    return (
+      <>
+        <Map
+          mouseEvents={true}
+          center={currLocation}
+          zoom={zoom}
+          width={600}
+          height={400}
+        >
+          <Marker
+            anchor={currLocation}
+            payload={1}
+            onClick={({ event, anchor, payload }) => {}}
+          />
+        </Map>
+        <button onClick={() => setZoom(zoom + 1)}>Zoom In</button>
+        <button onClick={() => setZoom(zoom - 1)}>Zoom Out</button>
+      </>
+    );
+  } else {
+    return <div>Getting the location data&hellip; </div>;
+  }
 };
 
-export default TicTacMap;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false
+  },
+  userDecisionTimeout: 5000
+})(TicTacMap);

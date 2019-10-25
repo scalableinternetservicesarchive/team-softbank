c7da1c2cd457b8359313b4773fa956b6834f7a7f
Kristie Lim <kristie.lim@gmail.com>
2019-10-25 14:33:49 -0700

    Zoom by buttons

--- app/javascript/components/TicTacMap.jsx
+++ app/javascript/components/TicTacMap.jsx
@@ -5,14 +5,26 @@ import Marker from 'pigeon-marker';
 
 const TicTacMap = props => {
   const { currLocation } = props;
+  const [zoom, setZoom] = React.useState(12);
+
   return (
-    <Map center={currLocation} zoom={12} width={600} height={400}>
-      <Marker
-        anchor={[50.874, 4.6947]}
-        payload={1}
-        onClick={({ event, anchor, payload }) => {}}
-      />
-    </Map>
+    <>
+      <Map
+        mouseEvents={true}
+        center={currLocation}
+        zoom={zoom}
+        width={600}
+        height={400}
+      >
+        <Marker
+          anchor={[50.874, 4.6947]}
+          payload={1}
+          onClick={({ event, anchor, payload }) => {}}
+        />
+      </Map>
+      <button onClick={() => setZoom(zoom + 1)}>Zoom In</button>
+      <button onClick={() => setZoom(zoom - 1)}>Zoom Out</button>
+    </>
   );
 };
 

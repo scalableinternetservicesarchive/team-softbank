7d58146dfb4cb4d5030de106971abc28243ac250
Richard Yang <richbluehill@gmail.com>
2019-10-31 19:58:23 -0700

    basic post user connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_025342) do
+ActiveRecord::Schema.define(version: 2019_11_01_025139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "author", null: false
     t.text "body"
     t.integer "likes"
     t.point "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.index ["author"], name: "index_posts_on_author"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,17 +3,13 @@
 # Table name: posts
 #
 #  id         :integer          not null, primary key
-#  author     :string           not null
 #  body       :text
 #  likes      :integer
 #  location   :point
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  title      :string
-#
-# Indexes
-#
-#  index_posts_on_author  (author)
+#  user_id    :integer
 #
 
 require 'rails_helper'

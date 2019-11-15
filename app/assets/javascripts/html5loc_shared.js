7c7839193eee2e45079632576b99d86bc55296c2
Alex Zhao <alexzhao21@gmail.com>
2019-11-15 15:03:37 -0800

    bound random locs to la-ish

--- app/assets/javascripts/html5loc_shared.js
+++ app/assets/javascripts/html5loc_shared.js
@@ -3,8 +3,13 @@ function getRandomInRange(from, to, fixed) {
 }
 
 function genRandomLoc() {
-  const latitude = getRandomInRange(-90.0, 90.0, 6);
-  const longitude = getRandomInRange(-180.0, 180.0, 6);
+  // around the entire planet
+  // const latitude = getRandomInRange(-90.0, 90.0, 6);
+  // const longitude = getRandomInRange(-180.0, 180.0, 6);
+
+  // within some random square around LA-ish
+  const latitude = getRandomInRange(33.69, 34.12, 6);
+  const longitude = getRandomInRange(-118.55, -118.08, 6);
 
   return { latitude, longitude };
 }

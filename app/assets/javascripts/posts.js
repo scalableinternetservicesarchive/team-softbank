a4669a661263e9cfc9724e199d8b096fc0b70674
Alex Zhao <alexzhao21@gmail.com>
2019-11-14 23:16:28 -0800

    error text value

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -10,6 +10,8 @@ function onSuccess(pos) {
 function onError() {
   document.getElementById('post_latitude').value = 'error';
   document.getElementById('post_longitude').value = 'error';
+
+  document.getElementById('locfield').innerText = 'Error getting location';
 }
 
 function setHTML5Loc() {

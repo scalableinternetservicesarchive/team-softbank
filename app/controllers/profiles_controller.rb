5433917686f6f13fba349bf737843e0325fec58a
Richard Yang <32914962+ryang72@users.noreply.github.com>
2019-11-19 23:02:36 -0800

    Smaller Issues (#68)
    * basic functionality to profile page
    
    * rubocop
    
    * favicon + default image + added number of comments to index
    
    * addressed pr requests
    
    * removed dupe spice count on posts

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,9 @@ class ProfilesController < ApplicationController
   def profile
     redirect_to '/users/sign_in' unless user_signed_in?
     @user = current_user
+    @posts = current_user.posts
+    @comments = current_user.comments
+    @total_recieved_spice = @posts.map { |p| p.votes_for.size }.reduce(:+)
+    + @comments.map { |c| c.votes_for.size }.reduce(:+)
   end
 end

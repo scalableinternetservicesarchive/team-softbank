79c4a574008c5b2ecacf32e333ad7b39db604985
Alex Zhao <alexzhao21@gmail.com>
2019-11-24 12:58:10 -0800

    add dist to posts

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,18 @@
 class ProfilesController < ApplicationController
+  before_action :location
+  
   def profile
     redirect_to '/users/sign_in' unless user_signed_in?
     @user = current_user
     @posts = current_user.posts
     @comments = current_user.comments
     @total_received_spice = @posts.sum { |p| p.likes.count } + @comments.sum { |c| c.likes.count }
+  end
+
+  private
+
+  def location
+    @location = session[:html5_geoloc]
+    @location ||= [0, 0] # TODO: something better
   end
 end

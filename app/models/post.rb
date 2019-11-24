e94ee04ebe82e91e88b4b5e01178309c05e6414f
Alex Zhao <alexzhao21@gmail.com>
2019-11-24 02:50:47 -0800

    let users see posts they made

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  body       :text
-#  likes      :integer
+#  like_count :integer          default("0")
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  title      :string

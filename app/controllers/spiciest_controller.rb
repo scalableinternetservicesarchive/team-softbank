827ea1854682f7357173b018b155cd2597ba38d9
Richard Yang <richbluehill@gmail.com>
2019-12-03 01:54:12 -0800

    added pagination to spiciest page

--- app/controllers/spiciest_controller.rb
+++ app/controllers/spiciest_controller.rb
@@ -1,5 +1,8 @@
 class SpiciestController < ApplicationController
+  POSTS_PER_PAGE = 25
   def spiciest
-    @posts = Post.spiciest
+    @spiciest_page = (params[:spiciest_page] || 1).to_i
+    @spiciest_page_max = 4 # lol
+    @posts = Post.spiciest.limit(POSTS_PER_PAGE).offset((@spiciest_page - 1) * POSTS_PER_PAGE)
   end
 end

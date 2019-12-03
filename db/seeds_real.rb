6537f4e64e58d4869088a146c6e3cfe26edd54e9
Alex Zhao <alexzhao21@gmail.com>
2019-12-03 13:30:59 -0800

    scale back to 10 users * 10 posts + 200 comments

--- db/seeds_real.rb
+++ db/seeds_real.rb
@@ -10,15 +10,15 @@ users = []
 posts = []
 comments = []
 
-# create 10 users, each with 10 posts
-10.times do
+# create 50 users, each with 50 posts
+50.times do
   u = User.create!(
     email: Faker::Internet.unique.email,
     password: '123456'
   )
   users.push(u.id)
 
-  10.times do
+  50.times do
     p = Post.create!(
       user_id: u.id,
       title: Faker::TvShows::SiliconValley.quote,
@@ -30,8 +30,8 @@ comments = []
   end
 end
 
-# create 200 random comments across all posts
-200.times do
+# create 5000 random comments across all posts
+5000.times do
   c = Comment.create!(
     user_id: users.sample,
     post_id: posts.sample,

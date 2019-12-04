a99659ace09f0c4c7c29d00bcf12aad2913175b3
Alex Zhao <alexzhao21@gmail.com>
2019-12-04 00:32:26 -0800

    might as well add it to profile too

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,9 +5,10 @@ class ProfilesController < ApplicationController
     return redirect_to '/users/sign_in' unless user_signed_in?
 
     @user = current_user
-    @posts = current_user.posts
-    @comments = current_user.comments
+    @posts = @user.posts
+    @comments = @user.comments
     @total_received_spice = @posts.sum { |p| p.likes.count } + @comments.sum { |c| c.likes.count }
+    fresh_when([@user.likes.count, @posts.all, @posts.sum(&:like_count), @posts.map { |p| helpers.distance_of_time_in_words(p.created_at, Time.now) }, @comments.all, @comments.sum(&:like_count), @comments.map { |c| helpers.distance_of_time_in_words(c.created_at, Time.now) }])
   end
 
   private

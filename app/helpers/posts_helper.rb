1a1d454bc1143e767be0240c666911d08e748088
Alex Zhao <alexzhao21@gmail.com>
2019-12-03 21:04:52 -0800

    good enuff

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,10 @@
 module PostsHelper
-  def cache_key_for_postcard(post, distance)
-    "postcard-#{post.id}-#{post.updated_at}-#{post.likes.count}-#{distance_of_time_in_words(post.created_at, Time.now)}-#{post.comments.count}-#{distance}"
+  def cache_key_for_postcard_table(posts, location, page_num, sort)
+    "postcard-table-page#{page_num}-sort#{sort}-#{posts.pluck(:updated_at).max}-#{distance_of_time_in_words(posts.pluck(:created_at).min, Time.now)}-#{posts.first.distance_to(location)}"
+  end
+
+  def cache_key_for_postcard(post, location)
+    "postcard-#{post.id}-#{post.updated_at}-#{post.likes.count}-#{distance_of_time_in_words(post.created_at, Time.now)}-#{post.comments.count}-#{post.distance_to(location)}"
   end
 
   def cache_key_for_noloc_postcard(post)

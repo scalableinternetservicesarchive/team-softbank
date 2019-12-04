a271fd79a6b87d792bdd1fd0b6d981ad84a6d6b1
Alex Zhao <alexzhao21@gmail.com>
2019-12-03 22:21:28 -0800

    2 * 5 + 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users = []
+posts = []
+comments = []
+
+# create 2 users, each with 5 posts
+2.times do
+  u = User.create!(
+    email: Faker::Internet.unique.email,
+    password: '123456'
+  )
+  users.push(u.id)
+
+  5.times do
+    p = Post.create!(
+      user_id: u.id,
+      title: Faker::TvShows::SiliconValley.quote,
+      body: Faker::Hipster.paragraph(sentence_count: 5),
+      latitude: rand(33.69..34.12),
+      longitude: rand(-118.55..-118.08)
+    )
+    posts.push(p.id)
+  end
+end
+
+# create 10 random comments across all posts
+10.times do
+  c = Comment.create!(
+    user_id: users.sample,
+    post_id: posts.sample,
+    body: Faker::Lorem.sentence(word_count: 8)
+  )
+  comments.push(c.id)
+end
+
+# TODO: likes
